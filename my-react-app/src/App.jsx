5350ce0a70ba35498dde6f1035957d6063408410
Ben Dang <s224116668@deakin.edu.au>
2024-04-28 15:26:02 +1000

    First commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="search-container">
+        <div className="nav-bar">
+          <h1>SipScore</h1>
+        </div>
+        <h1>Find the Best Cafe Near You!</h1>
+        <input
+          type="text"
+          className="search-box"
+          placeholder="Search for restaurants..."
+        />
+        <button className="search-button">Search</button>
+      </div>
+
+      <div className="restaurant-container">
+        <h2>Recently Reviewed Restaurants</h2>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+  return (
+    <div className="restaurant">
+      <h3>Restaurant 2</h3>
+      <p>Rating: 4.0</p>
+      <p>Location: Address 2</p>
+    </div>
+  );
+}
+export default Card;
